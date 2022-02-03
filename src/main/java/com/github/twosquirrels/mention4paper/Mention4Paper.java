e38e684f880a6d95dab697ba374fd9cf57fd8968
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-02-03 22:18:00 +0900

    Update: プラグイン名を McidMention から Mention4Paper に変更

--- src/main/java/com/github/twosquirrels/mention4paper/Mention4Paper.java
+++ src/main/java/com/github/twosquirrels/mention4paper/Mention4Paper.java
@@ -1,8 +1,8 @@
-package com.github.twosquirrels.mcidmention;
+package com.github.twosquirrels.mention4paper;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class McidMention extends JavaPlugin {
+public final class Mention4Paper extends JavaPlugin {
 
     @Override
     public void onEnable() {
